7c2887dd9684cd1b32d553ef63b70d5515fa1732
Mel Escolano <amalia.escolano@gmail.com>
2022-02-03 01:03:26 +0100

    Testing header and fixed herolist

--- components/detail.js
+++ components/detail.js
@@ -1,27 +1,38 @@
 import { Component } from "../components/component.js";
 import { Heroes } from "../heroes.js";
-
 export class Detail extends Component {
   template;
-  heroes;
+
   constructor() {
     super();
 
+    this.heroes = localStorage.getItem("heroes")
+      ? JSON.parse(localStorage.getItem("heroes"))
+      : Heroes;
+    let hero = this.wichHero();
+
     this.template = `
-      <h2>Heroes details</h2>
+      <h2>${hero.name} details</h2>
       <div>
-      <span></span>
+      <span>id: ${hero.id}</span>
       </div>
       <div>
       <label htmlFor="hero-name"></label>
-      <input type="text" />
+      <input type="text" value="${hero.name}"/>
       </div>
       <button>Go back</button>
       <button>save</button>
     `;
   }
   wichHero() {
-    const idHeroe = window.location.search;
-    console.log(idHeroe);
+    //Cogemos el ID de la url, filtramos con ese ID devolviendo el objeto
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const heroId = parseInt(urlParams.get("id"), 10);
+    console.log(heroId);
+    let filteredHeroes = this.heroes.filter((element) => {
+      return element.id === heroId;
+    });
+    return filteredHeroes[0];
   }
 }
--- components/header.spec.js
+++ components/header.spec.js
@@ -2,11 +2,18 @@ import { Header } from "./header.js";
 import { screen } from "@testing-library/dom";
 
 describe("testing Header", () => {
-  test("If header rendered", () => {
+  test("Testing h1 is rendered, should be Ok", () => {
     document.body.innerHTML = "<header id='header'></header>";
 
     new Header().render("#header");
 
     expect(screen.getByText(/Tour of Heroes/i)).toBeTruthy();
+  });
+  test("Testing h1 is rendered, should be Ok", () => {
+    document.body.innerHTML = "<header id='header'></header>";
+
+    new Header().render("#header");
+
+    expect(screen.getByRole("listitem", { name: "Dashboard" })).toBeTruthy();
   });
 });
